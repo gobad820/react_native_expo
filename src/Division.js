108c25ebcfebb890c49bf9876312f46212a05d62
Sanghae Kim <gobad820@gmail.com>
2023-08-02 23:40:08 +0900

    Kakao Division Line

---
+++ src/Division.js
@@ -0,0 +1,9 @@
+import {View} from "react-native";
+
+export const Division = (props) => {
+    return (
+        <View
+            style={{height: 0.5, backgroundColor: 'grey'}}
+        ></View>
+    )
+};
