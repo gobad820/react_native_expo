8eacfbe15c554adc85dd0dbe8c4ca0b24302d342
Sanghae Kim <gobad820@gmail.com>
2023-08-02 23:40:16 +0900

    Kakao Margin Space

---
+++ src/Margin.js
@@ -0,0 +1,9 @@
+import React from "react";
+import {View} from "react-native";
+
+export const Margin = (props) => {
+    return (
+        <View style={{height: props.height , backgroundColor: 'white'}}>
+        < /View>
+    )
+}
