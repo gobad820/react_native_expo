1152b4a0a4765a180b6fd0400a9cf4754aa3f38d
Sanghae Kim <gobad820@gmail.com>
2023-08-03 15:57:15 +0900

    Add FlatList

--- App.js
+++ App.js
@@ -1,30 +1,41 @@
-
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {Header} from './src/Header';
 import {Margin} from './src/Margin';
 import {Division} from './src/Division';
-import {SafeAreaView, View} from 'react-native';
+import {FlatList, View} from 'react-native';
 import {myProfile, friendProfiles} from './src/data';
-import Profile  from './src/Profile';
+import Profile from './src/Profile';
 import FriendSection from './src/FriendSection';
 import FriendList from './src/FriendList';
-
-
-
+import TabBar from "./src/TabBar";
+import MultiProfile from "./src/MultiProfile";
+import BirthdayProfile from "./src/BirthdayProfile";
 
 export default function App (){
 
     const [isOpened, setIsOpened] = useState(false)
+    const [selectedTabIdx, setSelectedTabIdx] = useState(0);
+
 
     const onPressArrow = ()=>{
         setIsOpened(!isOpened);
     }
 
+    const ItemSeparatorComponent = () => <Margin height ={13} />
 
+    const renderItem = ({item}) => (
+        <View>
+            <Profile
+                uri={item.uri}
+                name={item.name}
+                introduction={item.introduction} />
 
-    return (
-        <SafeAreaView style={{flex:1, paddingHorizontal:10}}>
-            <Margin height={10}/>
+        </View>
+    )
+
+    const ListHeaderComponent =()=>(
+        <View style={{flex:1, paddingHorizontal:10,backgroundColor:'white'}}>
+            <Margin height={50}/>
 
             <Header />
             <Profile
@@ -33,30 +44,38 @@ export default function App (){
                 introduction={myProfile.introduction}/>
             <Margin height={10}/>
             <Division />
+            <MultiProfile/>
+            <Division />
             <FriendSection
                 friendProfileLen={friendProfiles.length}
                 isOpened={isOpened}
                 onPressArrow={onPressArrow}/>
+        </View>
 
-            <FriendList data={friendProfiles} isOpened={isOpened}/>
-
-
-
-
-      {/*      { <Text>{count}</Text>*/}
-      {/*<Button title="더하기" onPress={()=>setCount(count+1)} />*/}
-      {/*<Margin height={5} />*/}
-      {/*<Button title="빼기" onPress={()=>setCount(count-1)} />*/}
-      {/*<Margin height={10}/> }*/}
-
-      {/*      {*/}
-      {/*       <FriendSection*/}
-      {/*  count = {friendProfiles.lengh}*/}
-      {/*  isOpened = {isOpened}*/}
-      {/*  onPressArrow = {onPressArrow}*/}
-      {/*/>*/}
-      {/*}*/}
-
-        </SafeAreaView>
-    );
+    )
+    const ListFooterComponent =()=>{
+        <View>
+            <TabBar selectedTabIdx={selectedTabIdx}
+                    setSelectedTabIdx={setSelectedTabIdx}/>
+        </View>
+    }
+    return (
+        <View
+            style={{flex:1}}>
+            {/* 해당 view 전체를 화면에 꽉 차게 두겠다. */}
+            <FlatList
+                data={isOpened ? friendProfiles : []}
+                contentContainerStyle={{paddingHorizontal:1}}
+                keyExtractor={(_, index)=>index}
+                stickyHeaderIndices={[0]}
+                ItemSeparatorComponent={ItemSeparatorComponent}
+                renderItem={renderItem}
+                ListHeaderComponent={ListHeaderComponent}
+                ListFooterComponent={ListFooterComponent}
+                showsVerticalScrollIndicator={false}
+            />
+            <TabBar selectedTabIdx={selectedTabIdx}
+                    setSelectedTabIdx={setSelectedTabIdx}/>
+        </ View>
+    )
 }
