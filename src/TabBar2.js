46cf6b17f82704072b17dd3cfcc5d7d8b14ededa
Sanghae Kim <gobad820@gmail.com>
2023-08-03 15:57:02 +0900

    Tabbar practice

---
+++ src/TabBar2.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {TouchableOpacity, View} from "react-native";
+import {getBottomSpace} from "react-native-iphone-x-helper";
+
+const bottomSpace = getBottomSpace()
+export default () =>{
+   <View
+       style={{flexDirection:'row'}}>
+       <TouchableOpacity>
+
+       </TouchableOpacity>
+   </View>
+}
