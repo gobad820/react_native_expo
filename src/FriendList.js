7bc12d1ab76c5da592dc6f15431772733503e166
Sanghae Kim <gobad820@gmail.com>
2023-08-03 15:56:37 +0900

    Modify Friends List Property

--- src/FriendList.js
+++ src/FriendList.js
@@ -1,23 +1,31 @@
 import React from 'react';
-import { ScrollView, View } from 'react-native';
+import {ScrollView, View} from 'react-native';
 import {Margin} from './Margin';
 import Profile from './Profile';
+import {getBottomSpace} from "react-native-iphone-x-helper";
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
 
-
+const bottomSpace = getBottomSpace()
 export default (props) => {
-    return (
-        <ScrollView showsVerticalScrollIndicator={true} contentContainerStyle={{ paddingBottom: 'bottomSpace' }}>
-            {props.data.map((item, index) => (
-                    <View key={index}>
+    return props.isOpened ? (
+        <ScrollView showsVerticalScrollIndicator={false}
+                    contentContainerStyle={{paddingBottom: bottomSpace}}>
+            {props.data.map((item, index) => {
+                    return (<View key={index}>
                         <Profile
                             uri={item.uri}
                             name={item.name}
                             introduction={item.introduction}
                         />
-                        <Margin height={13} />
-                    </View>
-                )
+                        <Margin height={13}/>
+                    </View>)
+                }
             )}
 
-        </ScrollView>
-    )};
+        </ScrollView>) :
+        <View
+        style={{paddingBottom:bottomSpace}}>
+
+        </View>;
+};
